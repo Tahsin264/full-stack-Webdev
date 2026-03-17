74c84481e447c954683bf7eafc03a6b744ed7683
Tahsin264 <t3616639@gmail.com>
2026-03-17 16:27:57 +0530

    initial commit

---
+++ functions.js
@@ -0,0 +1,15 @@
+// first of all lets see waht a normal function do i.e synchronous functions 
+
+// creating function for calculating the sum of numbers from 1 to n.
+
+function add(n){
+    let sum = 0;
+    for(let i = 1; i <= n; i++){
+        sum = sum + i;
+    }
+    return sum;
+}
+
+// calling of the function
+let ans = add(5);
+console.log(ans);
